a3d0ddd2fa8fc83f2b4441acf96dd3c991dfae83
jQ-0 <jerreteq@gmail.com>
2025-11-20 22:54:14 -0600

    Added logic

--- app/src/main/java/com/zybooks/dotty/DotsView.java
+++ app/src/main/java/com/zybooks/dotty/DotsView.java
@@ -1,4 +1,121 @@
 package com.zybooks.dotty;
 
-public class DotsView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+
+
+public class DotsView extends View {
+    public enum DotSelectionStatus {First, Additional, Last}
+
+    public interface DotsGridListener {
+        void onDotSelected(Dot dot, DotSelectionStatus status);
+    }
+
+    private final int DOT_RADIUS = 40;
+    private final DotsGame mGame;
+    private final Path mDotPath;
+    private DotsGridListener mGridListener;
+    private final int[] mDotColors;
+    private int mCellWidth;
+    private int mCellHeight;
+    private final Paint mDotPaint;
+    private final Paint mPathPaint;
+
+    public DotsView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+// Used to access the game state
+        mGame = DotsGame.getInstance();
+// Get color resources
+        mDotColors = getResources().getIntArray(R.array.dotColors);
+// For drawing dots
+        mDotPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+// For drawing the path between connected dots
+        mPathPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPathPaint.setStrokeWidth(10);
+        mPathPaint.setStyle(Paint.Style.STROKE);
+// The path between connected dots
+        mDotPath = new Path();
+    }
+
+    @Override
+    protected void onSizeChanged(int width, int height, int oldWidth, int oldHeight) {
+        int boardWidth = (width - getPaddingLeft() - getPaddingRight());
+        int boardHeight = (height - getPaddingTop() - getPaddingBottom());
+        mCellWidth = boardWidth / DotsGame.GRID_SIZE;
+        mCellHeight = boardHeight / DotsGame.GRID_SIZE;
+        resetDots();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+// Draw dots
+        for (int row = 0; row < DotsGame.GRID_SIZE; row++) {
+            for (int col = 0; col < DotsGame.GRID_SIZE; col++) {
+                Dot dot = mGame.getDot(row, col);
+                mDotPaint.setColor(mDotColors[dot.color]);
+                canvas.drawCircle(dot.centerX, dot.centerY, dot.radius, mDotPaint);
+            }
+        }
+// Draw connector between selected dots
+        ArrayList<Dot> selectedDots = mGame.getSelectedDots();
+        if (!selectedDots.isEmpty()) {
+            mDotPath.reset();
+            Dot dot = selectedDots.get(0);
+            mDotPath.moveTo(dot.centerX, dot.centerY);
+            for (int i = 1; i < selectedDots.size(); i++) {
+                dot = selectedDots.get(i);
+                mDotPath.lineTo(dot.centerX, dot.centerY);
+            }
+            mPathPaint.setColor(mDotColors[dot.color]);
+            canvas.drawPath(mDotPath, mPathPaint);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+// Only execute when a listener exists
+        if (mGridListener == null) return true;
+// Determine which dot is touched
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        int col = x / mCellWidth;
+        int row = y / mCellHeight;
+        Dot selectedDot = mGame.getDot(row, col);
+// Return previously selected dot if touch moves outside the grid
+        if (selectedDot == null) {
+            selectedDot = mGame.getLastSelectedDot();
+        }
+// Notify activity that a dot is selected
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            mGridListener.onDotSelected(selectedDot, DotSelectionStatus.First);
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            mGridListener.onDotSelected(selectedDot, DotSelectionStatus.Additional);
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            mGridListener.onDotSelected(selectedDot, DotSelectionStatus.Last);
+        }
+        return true;
+    }
+
+    public void setGridListener(DotsGridListener gridListener) {
+        mGridListener = gridListener;
+    }
+
+    private void resetDots() {
+        for (int row = 0; row < DotsGame.GRID_SIZE; row++) {
+            for (int col = 0; col < DotsGame.GRID_SIZE; col++) {
+                Dot dot = mGame.getDot(row, col);
+                dot.radius = DOT_RADIUS;
+                dot.centerX = col * mCellWidth + (mCellWidth / 2f);
+                dot.centerY = row * mCellHeight + (mCellHeight / 2f);
+            }
+        }
+    }
 }
--- app/src/main/java/com/zybooks/dotty/MainActivity.java
+++ app/src/main/java/com/zybooks/dotty/MainActivity.java
@@ -1,24 +1,66 @@
 package com.zybooks.dotty;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
+    private DotsGame mGame;
+    private DotsView mDotsView;
+    private TextView mMovesRemaining;
+    private TextView mScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        mMovesRemaining = findViewById(R.id.moves_remaining_text_view);
+        mScore = findViewById(R.id.score_text_view);
+        mDotsView = findViewById(R.id.dots_view);
+        mDotsView.setGridListener(mGridListener);
+        findViewById(R.id.new_game_button).setOnClickListener(this::newGameClick);
+        mGame = DotsGame.getInstance();
+        startNewGame();
+    }
+
+    private final DotsView.DotsGridListener mGridListener = new DotsView.DotsGridListener() {
+        @Override
+        public void onDotSelected(Dot dot, DotsView.DotSelectionStatus selectionStatus) {
+// Ignore selections when game is over
+            if (mGame.isGameOver()) return;
+// Add/remove dot to/from selected dots
+            DotsGame.DotStatus addStatus = mGame.processDot(dot);
+// If done selecting dots then replace selected dots and display new moves and score
+            if (selectionStatus == DotsView.DotSelectionStatus.Last) {
+                if (mGame.getSelectedDots().size() > 1) {
+                    mGame.finishMove();
+                    updateMovesAndScore();
+                } else {
+                    mGame.clearSelectedDots();
+                }
+            }
+// Display changes to the game
+            mDotsView.invalidate();
+        }
+    };
+
+    private void newGameClick(View view) {
+        startNewGame();
+    }
+
+    private void startNewGame() {
+        mGame.newGame();
+        mDotsView.invalidate();
+        updateMovesAndScore();
+    }
+
+    private void updateMovesAndScore() {
+        mMovesRemaining.setText(String.format(Locale.getDefault(), "%d", mGame.getMovesLeft()));
+        mScore.setText(String.format(Locale.getDefault(), "%d", mGame.getScore()));
     }
 }
